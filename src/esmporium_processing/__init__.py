b8734fcca843f9f81024bcb84436d96c4f1a81af
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2026-09-06 12:11:05 +1000

    Setup with copier
    Add uv.lock
    Add requirements files

---
+++ src/esmporium_processing/__init__.py
@@ -0,0 +1,7 @@
+"""
+Processing of datasets retrieved with esmporium (https://github.com/esmporium/esmporium).
+"""
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("esmporium_processing")
